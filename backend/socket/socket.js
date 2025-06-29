40be708fcaca4b0fc8a3e2e8461ecf7d0179cc50
DILIP-SURYAL <dilipsuriyal@gmail.com>
2025-06-29 18:19:07 +0530

    frontend is deployed

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ let app = express();
 const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // ✅ set your actual frontend origin
+    origin: process.env.CLIENT_URL,
     credentials: true, // ✅ allow credentials
   },
 });
